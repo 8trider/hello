97d8046a81df33da2d12125d15ab7ef8905f59da
8trider <8trider@example.com>
2023-10-22 15:10:40 +0900

    edited

--- src/hello_8trider/who.py
+++ src/hello_8trider/who.py
@@ -1,5 +1,8 @@
 def my_name():
     print("My name is H8 ")
 
-def eight():
-    print("8 "*8)
+def lotto():
+    import random
+    numbers = list(range(1, 43))
+    random_numbers = random.sample(numbers, 6)
+    print(random_numbers)
